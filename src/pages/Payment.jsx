12b353955b462b41d417ecbfe354214d534023f9
sanam-rai356 <sm8027848@gmail.com>
2025-01-28 08:45:52 +0545

    change the source folder in payment

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -83,14 +83,14 @@ const Payment = () => {
             type="hidden"
             id="success_url"
             name="success_url"
-            value="http://localhost:5173/success"
+            value="https://food-website-three-kappa.vercel.app/success"
             required
           />
           <input
             type="hidden"
             id="failure_url"
             name="failure_url"
-            value="http://localhost:5173/failure"
+            value="https://food-website-three-kappa.vercel.app/failure"
             required
           />
           <input
