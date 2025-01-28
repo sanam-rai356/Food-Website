dc06a50652f951f40dba5e9f820d24837792fbeb
sanam-rai356 <sm8027848@gmail.com>
2025-01-28 12:14:26 +0545

    menupara

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -19,9 +19,6 @@ const Menu = () => {
 
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum magnam
-      quas fugiat sequi inventore exercitationem molestiae pariatur similique
-      debitis?
       <section className="mt-[87px] py-10 main_padding ">
         <div className="w-full">
           <h3 className="text-center text-3xl mb-8 md:text-4xl font-semibold">
@@ -60,6 +57,29 @@ const Menu = () => {
             </div>
           )}
         </div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
+        explicabo ipsum nisi. Quo exercitationem magni quas beatae quis nemo ea
+        sapiente, dolore, debitis fugiat consectetur aspernatur molestias omnis
+        expedita architecto delectus possimus officiis vitae doloribus
+        perspiciatis reiciendis. Ad consequuntur voluptates accusantium
+        laboriosam laborum qui, culpa vel molestiae perspiciatis dolorem ex,
+        eaque illo odit laudantium, recusandae aliquam. Provident ipsa
+        praesentium libero veniam, quis tempore mollitia impedit totam esse
+        ipsam voluptates repudiandae corrupti molestiae asperiores et dolore
+        fugiat eligendi distinctio quia? Amet necessitatibus dolor nemo nostrum
+        veniam ea praesentium aut, ex ab id cum, voluptate non? Dolorum aliquid
+        non quisquam repellat dolores dolorem perspiciatis dolor! Neque
+        doloribus culpa quae illo ab eveniet voluptates quisquam. Repudiandae
+        quis exercitationem quaerat dolorum omnis reiciendis harum, eaque
+        similique vitae magni. Incidunt voluptatibus officia nihil? Tempore
+        porro, praesentium deserunt sequi quia ipsum incidunt, doloribus quas
+        delectus accusamus odit. Rerum, tenetur. Minus possimus aliquid
+        nesciunt, voluptate laboriosam hic tempore reiciendis ratione ducimus ex
+        enim iste eius ullam eos aspernatur blanditiis pariatur? Dicta velit
+        accusamus vel quod, sequi nobis praesentium quam voluptatibus in facilis
+        sed nihil, vitae totam fugit eos atque laborum natus. Vel autem sint ad
+        sed, sit tenetur asperiores necessitatibus atque rem temporibus deleniti
+        laborum labore voluptate.
       </section>
     </>
   );
