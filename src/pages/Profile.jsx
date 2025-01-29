4d490cc17232503984160757b6467ae472e55233
sanam-rai356 <sm8027848@gmail.com>
2025-01-29 08:45:35 +0545

    profileChanges1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,17 +8,23 @@ const Profile = () => {
   return (
     <>
       {isAuthenticated ? (
-        <section className="main_padding mt-[87px]">
-          <h1>Welcome {user.name}</h1>
-          <img src={user.picture} alt="user picture" />
-          <p>Email: {user.email}</p>
-          <button
-            onClick={() => {
-              logout();
-            }}
-          >
-            Logout
-          </button>
+        <section className="main_padding mt-[87px] bg-[#a7a7a7] flex flex-col items-center justify-center">
+          <div className="bg-white w-[50%] mx-auto rounded-lg shadow-md p-5 flex flex-col items-center justify-center gap-4">
+            <h1>Welcome {user.name}</h1>
+            <img
+              src={user.picture}
+              alt="user picture"
+              className="size-24 rounded-full border-[2px]"
+            />
+            <p>Email: {user.email}</p>
+            <button
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </button>
+          </div>
         </section>
       ) : (
         <div>Loading...</div>
