18ca6cab7d7748bc5195927d860f050752b9f8f7
sanam-rai356 <sm8027848@gmail.com>
2025-01-29 08:50:22 +0545

    profile-fonts

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,13 +10,14 @@ const Profile = () => {
       {isAuthenticated ? (
         <section className="main_padding mt-[87px] h-[70vh] bg-[#a7a7a7] flex flex-col items-center justify-center">
           <div className="bg-white w-[50%] mx-auto rounded-lg shadow-md p-5 flex flex-col items-center justify-center gap-4">
-            <h1>Welcome {user.name}</h1>
+            <h1 className="text-3xl font-semibold">Welcome</h1>
             <img
               src={user.picture}
               alt="user picture"
               className="size-24 rounded-full border-[2px]"
             />
-            <p>Email: {user.email}</p>
+            <h2 className="text-3xl font-semibold">{user.name}</h2>
+            <p className="text-xl">Email: {user.email}</p>
             <button
               onClick={() => {
                 logout();
